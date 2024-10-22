a14a7f9112ca47c763e4735216394dffc3ba7557
LieblingElaina <3193377542@qq.com>
2024-10-22 17:15:28 +0800

    change2

---
+++ layouttest/app/src/main/java/com/example/layouttest/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.layouttest;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.layouttest.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.linearlayout);
+    }
+}
